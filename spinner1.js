aa1f228b1fad4afe436663bf522d7a8fa8d2a2b9
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-10 14:02:33 -0400

    Implemented spinner1

---
+++ spinner1.js
@@ -0,0 +1,33 @@
+// Trio programmed with @Linh095 and @BilliMorgan
+
+process.stdout.write("hello from spinner1.js... \rheyyy\n");
+
+const interval = 200;
+let timeout = 0;
+
+for (let i = 0; i < 2; i++) {
+
+  setTimeout(() => {
+    process.stdout.write("\r|   ");
+  }, timeout += interval);
+
+  setTimeout(() => {
+    process.stdout.write("\r/   ");
+  }, timeout += interval);
+
+  setTimeout(() => {
+    process.stdout.write("\r-   ");
+  }, timeout += interval);
+
+  setTimeout(() => {
+    // Need to escape the backslash since it"s a special character.
+    process.stdout.write("\r\\   ");
+  }, timeout += interval);
+
+}
+
+setTimeout(() => {
+  process.stdout.write("\n");
+}, timeout += interval);
+
+console.log("Done queueing up timeouts.");
